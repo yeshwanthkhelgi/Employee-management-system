86c27c3e8a036243fcfc609c88ec346f7a71efad
yeshwanthkhelgi <yeshwanthkhelgi@gmail.com>
2025-03-13 18:51:57 +0530

    changed package name

--- src/main/java/com/yeshwanth/employeemanagementsystem/domain/Employee.java
+++ src/main/java/com/yeshwanth/employeemanagementsystem/domain/Employee.java
@@ -1,4 +1,4 @@
-package com.satoruakiyama.employeemanagementsystem.domain;
+package com.yeshwanth.employeemanagementsystem.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
